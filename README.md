8eff6fd9e12f6694b151abc2307cd8cdba71a729
Isaac Creer <isaaccreer@gmail.com>
2023-08-10 09:40:27 -0600

    Small update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mesh_generation
+This program is for mesh generation using pyvista 
+
+## Mesh that can be Generated
+- 2-D Shapes
+-- Square
+--Cirle 
+-- Octigon 
---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+import numpy as np
+import pyvista as vista 
+
+def option():
+    dementions = input("Please select the type of mesh you want generated: \n\t1.2-D \n\t3-D")
+    if ( dementions == 1):
+        shape = input("")
+    elif (dementions == 2):
+        shape = input("Please select a shape: \n1.Sphere \n2.Cube")
+
+def main():
+    print("hold")
+
+
+if __name__ == '__main__':
+    main()
