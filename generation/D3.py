6027cb60a2cd93f1c8184e21d879aad9e7cbeb90
Isaac Creer <isaaccreer@gmail.com>
2023-08-14 11:38:29 -0600

    Got sphere point stated

--- generation/D3.py
+++ generation/D3.py
@@ -33,8 +33,17 @@ def cube():
     return box
 
 def sphere():
-    s = vista.Sphere()
-    p = vista.Plotter(shape=(1,1))
-    p.subplot(0,0)
-    p.add_mesh(s, color='lightblue', show_edges=True)
-    return p
+
+    number_of_points = 100
+    outer_factor = 1
+    inter_facotr = 0.95
+    outer_points = []
+    inter_points = []
+    for i in range(number_of_points):
+        outer_points.append([outer_factor*np.cos((i*2*np.pi)/number_of_points),outer_factor*np.sin((i*2*np.pi)/number_of_points), outer_factor*np.cos((i*np.pi)/(number_of_points))])
+        inter_points.append([inter_facotr*np.cos((i*2*np.pi)/number_of_points),inter_facotr*np.sin((i*2*np.pi)/number_of_points), inter_facotr*np.cos((i*np.pi)/(number_of_points))])
+        
+    print(outer_points)
+    print(inter_points)
+
+    return 0
